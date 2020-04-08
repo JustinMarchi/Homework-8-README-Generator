a2e27f0e74d84902c0a0b56f07f3710d33de72b8
Justin Marchi <marchij86@gmail.com>
2020-04-08 14:35:40 -0400

    Locked token in .env and added it to .gitignore. Also added a working image and email generation

--- utils/api.js
+++ utils/api.js
@@ -1,8 +1,10 @@
 const axios = require("axios");
+require('dotenv').config();
 
+const token = process.env.TOKEN;
 const api = {
  async getUser(username) {
-    const res = await axios({url: `https://api.github.com/users/${username}?access_token=821e9a6165e945f26d29c07b572aece96049fe89`, method: 'GET'}) 
+    const res = await axios({url: `https://api.github.com/users/${username}?access_token=${token}`, method: 'GET'}) 
     const user = res.data
     console.log(user);
     return user;
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,25 +2,28 @@ const api = require("./api");
 
 function generateMarkdown(answers, user) {
   return `
-  #${user.name}
-  #${user.avatar_url}
-
+  ![](${user.avatar_url})
+  # ${user.name}
+  ### [E-mail Me Here!](${user.email})
+---
   # ${answers.ProjectTitle}
 
+---
 ## Description 
 ${answers.ProjectDescription}
-
+---
 
 ## Table of Contents
 
 * [Installation](#installation)
 * [Usage](#usage)
 
-
+---
 ## Installation
 
 ${answers.Install}
 
+---
 ## Usage 
 
 ${answers.Usage}
